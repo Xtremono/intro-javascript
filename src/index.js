eb99db711da11257d1412523ee5171f26951a041
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-11-06 13:35:49 -0300

    destructuracion

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-console.log("Hola nada");
+//1.Desestructuracion
+//2.Asignacion desestructurante
 
+const persona = {
+    nombre: "Tomas",
+    edad: 31,
+    clave: "Ironman",
+};
+
+//const { nombre, clave } = persona;
+
+//console.log(persona.nombre);
+//console.log(persona.edad);
+//console.log(clave);
+//console.log(nombre);
+
+const getPersona = ({nombre}) => {
+    console.log(nombre)
+};
+
+getPersona(persona);
